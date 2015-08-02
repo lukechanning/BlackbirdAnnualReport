795793fc286f02a5a8e3353ad9e9f4ac0058d6ab
Luke Patrick <luke@snodandjeff.com>
2015-08-02 05:36:24 +0000

    Updates layout to match requirements, introducts brand-new and somewhat ugly jQuery to only show LazyLine Painter on scroll

--- site.js
+++ site.js
@@ -12,6 +12,147 @@ $('#heart').animate(
     }, 1000, function(){pulse(!back)});
 })(false);
 
+//Introduce LazyLine Painter
+/*
+ * Lazy Line Painter
+ * SVG Stroke animation.
+ *
+ * https://github.com/camoconnell/lazy-line-painter
+ * http://www.camoconnell.com
+ *
+ * Licensed under the MIT license.
+ *
+ */
+(function(e){var g={init:function(a){return this.each(function(){var b=e(this),c=b.data("lazyLinePainter");b.addClass("lazy-line");if(!c){var c=e.extend({width:null,height:null,strokeWidth:2,strokeColor:"#000",strokeOverColor:null,strokeCap:"round",strokeJoin:"round",strokeOpacity:1,arrowEnd:"none",onComplete:null,onStart:null,delay:null,overrideKey:null,drawSequential:!0,speedMultiplier:1,reverse:!1,responsive:!1},a),d=c.overrideKey?c.overrideKey:b.attr("id").replace("#",""),f=c.svgData[d].dimensions.width,
+l=c.svgData[d].dimensions.height;c.svgData=c.svgData[d].strokepath;null===c.width&&(c.width=f);null===c.height&&(c.height=l);c.responsive||b.css({width:c.width,height:c.height});d="0 0 "+f+" "+l;f=document.createElementNS("http://www.w3.org/2000/svg","svg");f.setAttributeNS(null,"viewBox",d);f.setAttribute("xmlns","http://www.w3.org/2000/svg");c.svg=e(f);b.append(c.svg);b.data("lazyLinePainter",c)}})},paint:function(){return this.each(function(){var a=e(this).data("lazyLinePainter"),b=function(){a.paths=
+[];a.longestDuration=0;for(var b=a.playhead=0,d=0,f=0,d=0;d<a.svgData.length;d++)b=a.svgData[d].duration*a.speedMultiplier,f+=b;for(d=0;d<a.svgData.length;d++){var e=m(a,d),h=e.getTotalLength();e.style.strokeDasharray=h+" "+h;e.style.strokeDashoffset=h;e.style.display="block";e.getBoundingClientRect();b=a.svgData[d].duration*a.speedMultiplier;b>a.longestDuration&&(a.longestDuration=b);var g;g=a.reverse?f-=b:a.playhead;a.paths.push({duration:b,drawStartTime:g,path:e,length:h});a.playhead+=b}a.totalDuration=
+a.drawSequential?a.playhead:a.longestDuration;a.rAF=requestAnimationFrame(function(b){k(b,a)});if(null!==a.onStart)a.onStart()};null===a.delay?b():setTimeout(b,a.delay)})},pauseResume:function(){return this.each(function(){var a=e(this).data("lazyLinePainter");a.paused?(a.paused=!1,requestAnimationFrame(function(b){n(b,a)})):(a.paused=!0,cancelAnimationFrame(a.rAF))})},erase:function(){return this.each(function(){var a=e(this).data("lazyLinePainter");a.startTime=null;a.elapsedTime=null;cancelAnimationFrame(a.rAF);
+a.svg.empty()})},destroy:function(){return this.each(function(){var a=e(this);a.removeData("lazyLinePainter");a.remove()})}},n=function(a,b){b.startTime=a-b.elapsedTime;requestAnimationFrame(function(a){k(a,b)})},k=function(a,b){b.startTime||(b.startTime=a);b.elapsedTime=a-b.startTime;for(var c=0;c<b.paths.length;c++){var d;b.drawSequential?(d=b.elapsedTime-b.paths[c].drawStartTime,0>d&&(d=0)):d=b.elapsedTime;d<b.paths[c].duration&&0<d?(d=d/b.paths[c].duration*b.paths[c].length,b.paths[c].path.style.strokeDashoffset=
+b.reverse||b.svgData[c].reverse?-b.paths[c].length+d:b.paths[c].length-d):d>b.paths[c].duration&&(b.paths[c].path.style.strokeDashoffset=0)}if(b.elapsedTime<b.totalDuration)b.rAF=requestAnimationFrame(function(a){k(a,b)});else if(null!==b.onComplete)b.onComplete()},m=function(a,b){var c=document.createElementNS("http://www.w3.org/2000/svg","path"),d=e(c);a.svg.append(d);d.attr(p(a,a.svgData[b]));return c},p=function(a,b){return{d:b.path,stroke:b.strokeColor?b.strokeColor:a.strokeColor,"fill-opacity":0,
+"stroke-opacity":b.strokeOpacity?b.strokeOpacity:a.strokeOpacity,"stroke-width":b.strokeWidth?b.strokeWidth:a.strokeWidth,"stroke-linecap":b.strokeCap?b.strokeCap:a.strokeCap,"stroke-linejoin":b.strokeJoin?b.strokeJoin:a.strokeJoin}};e.fn.lazylinepainter=function(a){if(g[a])return g[a].apply(this,Array.prototype.slice.call(arguments,1));if("object"!==typeof a&&a)console.log("opps - issue finding method");else return g.init.apply(this,arguments)}})(jQuery);
+
+//PUll the trigger on SVG Animation
+
+
+var pathObj = {
+    "icons": {
+        "strokepath": [
+            {
+                "path": "M 169.92 338.15 L 203.43 338.15",
+                "duration": 200
+            },
+            {
+                "path": "M 169.92 323.82 L 203.43 323.82",
+                "duration": 200
+            },
+            {
+                "path": "M 179.4 352.27 L 193.95 352.27",
+                "duration": 200
+            },
+            {
+                "path": "M 180.14,319.39 L 180.14,295.15      193.21,295.15 193.21,319.391    ",
+                "duration": 200
+            },
+            {
+                "path": "M179.06,316.62     c-14.84-3.45-25.89-16.75-25.89-32.64c0-18.51,15-33.51,33.51-33.51c18.51,0,33.51,15,33.51,33.51     c0,15.8-10.93,29.04-25.64,32.581",
+                "duration": 200
+            },
+            {
+                "path": "M 338.17,253.2 L 354.81,269.86      281.79,342.891 260.19,348.05 265.24,326.13 338.17,253.21    ",
+                "duration": 200
+            },
+            {
+                "path": "M 318.14 274.08 L 333.74 289.67",
+                "duration": 200
+            },
+            {
+                "path": "M 270.25 320.5 L 287.25 337.75",
+                "duration": 200
+            },
+            {
+                "path": "M 424.32,355.0 L 394.81,325.71      479.12,241.41 508.62,270.71 424.32,355.01    ",
+                "duration": 200
+            },
+            {
+                "path": "M 426.01 322.76 L 441.25 338",
+                "duration": 200
+            },
+            {
+                "path": "M 454.67 294.1 L 470 309.5",
+                "duration": 200
+            },
+            {
+                "path": "M 480.28 268.49 L 495.5 283.5",
+                "duration": 200
+            },
+            {
+                "path": "M 436.23 303.27 L 455.75 323.5",
+                "duration": 200
+            },
+            {
+                "path": "M 462.15 277.34 L 482 297.25",
+                "duration": 200
+            },
+            {
+                "path": "M 626.18,255.9 L 641.35,255.95      635.45,290.73 646.83,298.52 636.3,306.109 639.88,339.83 626.39,339.83    ",
+                "duration": 200
+            },
+            {
+                "path": "M 569.28,255.9 L 554.1,255.95      560,290.73 548.62,298.52 559.16,306.109 555.58,339.83 569.07,339.83    ",
+                "duration": 200
+            },
+            {
+                "path": "M597.1,274.71c4.41,0,7.98,3.57,7.98,7.98     s-3.58,7.98-7.98,7.98c-4.399,0-7.979-3.57-7.979-7.98C589.11,278.28,592.69,274.71,597.1,274.71",
+                "duration": 200
+            },
+            {
+                "path": "M597.1,309.01c4.41,0,7.98,3.57,7.98,7.98     s-3.58,7.98-7.98,7.98c-4.399,0-7.979-3.58-7.979-7.98C589.11,312.58,592.69,309.01,597.1,309.01",
+                "duration": 200
+            },
+            {
+                "path": "M 603.22 322.09 L 593.72 333.58",
+                "duration": 200
+            }
+        ],
+        "dimensions": {
+            "width": 800,
+            "height": 600
+        }
+    }
+}; 
+ 
+//Add Appear Script
+function isScrolledIntoView(elem)
+{
+    var $elem = $(elem);
+    var $window = $(window);
+
+    var docViewTop = $window.scrollTop();
+    var docViewBottom = docViewTop + $window.height();
+
+    var elemTop = $elem.offset().top;
+    var elemBottom = elemTop + $elem.height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+ 
+/* 
+Make that animation happen! 
+ */ 
+ 
+$(window).scroll(function() {
+     if( isScrolledIntoView('#icons') ) {
+             $('#icons').lazylinepainter( 
+             {
+                "svgData": pathObj,
+                "strokeWidth": 10,
+                "strokeColor": "#262213"
+            }).lazylinepainter('paint');
+    } else {
+     
+    }
+});
+ 
 //Pull the trigger on Slick
 
 $('.success-slider').slick({
